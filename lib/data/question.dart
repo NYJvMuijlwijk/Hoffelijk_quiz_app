fed087661165f76c4d2910485c64baaaa07780fb
Niek <microniek@live.nl>
2021-10-14 17:50:59 +0200

    question class and qubit added

---
+++ lib/data/question.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class Question extends Equatable {
+  final String question;
+  final String answer;
+  final List<String> secundaryAnswers;
+
+  const Question({
+    this.question = "question",
+    this.answer = "answer",
+    this.secundaryAnswers = const ["1", "2", "3"],
+  });
+
+  @override
+  List<Object> get props => [question, answer, secundaryAnswers];
+
+  Question copyWith({
+    String? question,
+    String? answer,
+    List<String>? secundaryAnswers,
+  }) {
+    return Question(
+      question: question ?? this.question,
+      answer: answer ?? this.answer,
+      secundaryAnswers: secundaryAnswers ?? this.secundaryAnswers,
+    );
+  }
+}
