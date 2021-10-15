6c56a353ce85cd62651db49a0e6cd720a4e9afe3
Niek <microniek@live.nl>
2021-10-15 16:43:04 +0200

    score added to question

--- lib/data/question.dart
+++ lib/data/question.dart
@@ -4,27 +4,31 @@ class Question extends Equatable {
   final String question;
   final String answer;
   final List<String> secundaryAnswers;
+  final int score;
 
   const Question({
     this.question = "question",
     this.answer = "answer",
-    this.secundaryAnswers = const ["1", "2", "3"],
+    this.secundaryAnswers = const ["1", "2"],
+    this.score = 1,
   });
 
   List<String> getAllAnswers() => [answer, ...secundaryAnswers];
 
   @override
-  List<Object> get props => [question, answer, secundaryAnswers];
+  List<Object> get props => [question, answer, secundaryAnswers, score];
 
   Question copyWith({
     String? question,
     String? answer,
     List<String>? secundaryAnswers,
+    int? score,
   }) {
     return Question(
       question: question ?? this.question,
       answer: answer ?? this.answer,
       secundaryAnswers: secundaryAnswers ?? this.secundaryAnswers,
+      score: score ?? this.score,
     );
   }
 }
